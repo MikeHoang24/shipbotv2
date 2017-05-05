5a127de06dc7d79b2b1b2455d0f30d56c2bd6a0e
MikeHoang24 <dhoang@andrew.cmu.edu>
2017-05-04 20:18:03 -0400

    05042017

---
+++ code/audio.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 04 13:57:02 2017
+
+@author: Michu
+"""
+
+import pyaudio as pa
+import wave
+from pygame import mixer as mix
+import time
+
+CHUNK = 1024
+
+class audioControl():
+    def __init__(self):
+        self.maxChannels = mix.get_num_channels()
+        self.info = 0
+        self.music = 1
+        
+    def play(self, fname):
+        fname = "sound/" + fname + ".wav"
+        mix.Channel(self.info).queue(mix.Sound(fname))
+        
+    def playMusic(self, fname):
+        fname = "sound/" + fname + ".wav"
+        mix.Channel(self.music).queue(mix.Sound(fname))
+        
+    def play_station(self,letter):
+        self.play("station")
+        self.play(letter)
+        
+    def lol(self):
+        print("heh")
+    
+    def play_device(self,device):
+        #self.play("device")
+        mix.music.queue('sound/'+device+'.wav')
+        mix.music.queue('sound/station.wav')
+        mix.music.queue('sound/'+device+'.wav')
+        if device == "V1":
+            fname = "small"
+        elif device == "V2":
+            fname = "big"
+        elif device == "V3":
+            fname = "shuttlecock"
+        elif device == "A" or device == "B":
+            fname = "breaker"
+        else:
+            fname = "error"
+
+        self.play(fname)
+        if device == "A":
+            self.play("A")
+        elif device == "B":
+            self.play("B")
+    
+
+    def old_play(self,fname):
+        
+        fname = "sound/" + fname +".wav"
+        
+        wf = wave.open(fname, 'rb')
+        
+        p = pa.PyAudio()
+        
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+        
+        data = wf.readframes(CHUNK)
+        
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(CHUNK)
+        
+        stream.stop_stream()
+        stream.close()
+        
+        p.terminate()
+
+#play("station")
+#play("device")
+audiocontrol = audioControl()
+audiocontrol.play_device("A")
--- code/test.py
+++ code/test.py
@@ -1,33 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 26 23:24:52 2017
+import multiprocessing
 
-@author: Michu
-"""
+def worker(num):
+    """thread worker function"""
+    print 'Worker:', num
+    return
 
-import serial
-import time
-
-drive_port = "COM6"
-stepper_port = "COM3"
-
-ser = serial.Serial(drive_port, 9600, timeout=5)
-time.sleep(1)
-ser.write("f\n")
-line = ser.readline()
-while not ("DONE" in line):
-    line = ser.readline()
-print(line)
-print ("drive test complete")
-ser.close()
-
-
-
-ser2 = serial.Serial(stepper_port, 9600, timeout=5)
-time.sleep(2)
-ser2.write("zi")
-ser2.flush()
-line = ser2.readline()
-print(line)
-print("stepper test complete")
-ser2.close()
+if __name__ == '__main__':
+    jobs = []
+    for i in range(5):
+        p = multiprocessing.Process(target=worker, args=(i,))
+        jobs.append(p)
+        p.start()
